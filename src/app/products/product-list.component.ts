c5bc31abb9344667614e751a3ee943aec2284e11
diegochagas <drochachagas@gmail.com>
2019-03-04 14:52:55 -0300

    Observable to http client products request added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
     productHeight: string = '50';
     showImage: boolean = false;
     filteredProducts: IProduct[];
+    errorMessage: string;
     
     _listFilter: string;
 
@@ -37,8 +38,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+      this.productService.getProducts().subscribe(
+        products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error => this.errorMessage = <any>error
+      );
     }
 
     onRatingClicked(message: string): void {
