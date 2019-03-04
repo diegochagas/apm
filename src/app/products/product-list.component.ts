472df6f7d65ddba965b28859d0b3bdc973dfe066
diegochagas <drochachagas@gmail.com>
2019-03-03 22:21:55 -0300

    Component of rating stars included

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -92,4 +92,8 @@ export class ProductListComponent implements OnInit {
     ngOnInit(): void {
         console.log('In OnInit');
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = `Product list: ${message}`;
+    }
 }
