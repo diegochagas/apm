7c1d0149d766320cef415be8fa04876bd5dccefb
diegochagas <drochachagas@gmail.com>
2019-03-05 00:39:26 -0300

    product detail component implemented

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,12 +11,22 @@ import { IProduct } from '../product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product detail";
   product: IProduct;
+  errorMessage: string = '';
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) {}
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
+    if(id) {
+      this.getProduct(id);
+    }
+  }
+  
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error
+    );
   }
 
   onBack(): void {
